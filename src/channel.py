daed51bbd856a643a40ce2f602f824ca5faadd4d
Mamed Mamedov <demam777@gmail.com>
2023-11-09 11:39:35 +0300

    add

--- src/channel.py
+++ src/channel.py
@@ -8,7 +8,7 @@ from googleapiclient.discovery import build
 class Channel:
     """Класс для ютуб-канала"""
 
-    api_key = 'AIzaSyDNal8EcMuEsVzrNkIdbkMWo2d_Nu6w2PI'
+    api_key = 'AIzaSyBye8OSheiQHAy-_tHT7JKneRcHzc9hcig'
     youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, channel_id: str) -> None:
@@ -25,3 +25,15 @@ class Channel:
         """Выводит в консоль информацию о канале"""
         channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
         print(json.dumps(channel, indent=2, ensure_ascii=False))
+
+
+class Video:
+    def __init__(self, video_id: str):
+        self.id_video = video_id
+        video_response = self.get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                          id=video_id).execute()
+        self.title: str = video_response['items'][0]['snippet']['title']
+
+    @classmethod
+    def get_service(cls):
+        return build('youtube', 'v3', developerKey=os.getenv('AIzaSyDNal8EcMuEsVzrNkIdbkMWo2d_Nu6w2PI'))
